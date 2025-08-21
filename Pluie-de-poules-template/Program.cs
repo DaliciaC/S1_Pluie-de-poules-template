f1d68a0efb19db48a3035e7ed292d772d3f4eff6
Dalicia Bouallouche <dalicia.bouallouche@cegepoutaouais.qc.ca>
2025-08-21 09:52:41 -0400

    Quelques changement d'ordre de lignes de code pour les indices

--- Pluie-de-poules-template/Program.cs
+++ Pluie-de-poules-template/Program.cs
@@ -6,7 +6,15 @@
     {
         // Point d'entrée du programme
 
+        //Hauteur de la fenêtre console en nombre de lignes
+        Console.WindowHeight
+
         int sol = Console.WindowHeight - 2;
+
+        //Positionner le curseur sur une position spécifique dans la console:
+        Console.SetCursorPosition(x, y);
+        //x = la colonne(de gauche à droite, commence à 0).
+        //y = la ligne(de haut en bas, commence à 0).
 
         // Configuration pour afficher les emojis
         Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -19,12 +27,5 @@
         // Effacer la console 
         Console.Clear();
 
-        //Hauteur de la fenêtre console en nombre de lignes
-        Console.WindowHeight
-
-        //Positionner le curseur sur une position spécifique dans la console:
-        Console.SetCursorPosition(x, y);
-        //x = la colonne(de gauche à droite, commence à 0).
-        //y = la ligne(de haut en bas, commence à 0).
     }
 }
